d9ae2207724407d8bac6db4a8ed678c2dc4cecf0
PiotrBienkowski <piotr.j.bienkowski@gmail.com>
2024-02-24 14:03:48 +0100

    Added python scripts

---
+++ scripts/get_names.py
@@ -0,0 +1,13 @@
+import os
+import json
+
+folder_path = 'new'
+
+output_file = 'items_out.json'
+
+files_in_folder = [file for file in os.listdir(folder_path) if os.path.isfile(os.path.join(folder_path, file))]
+
+with open(output_file, 'w') as file:
+    json.dump(files_in_folder, file)
+
+print(f"Zapisano nazwy plików do {output_file}.")
---
+++ scripts/pro.py
@@ -0,0 +1,25 @@
+import os
+import shutil
+import json
+
+org_folder = 'org'
+new_folder = 'new'
+filter_file = 'items.json'
+
+if not os.path.exists(new_folder):
+    os.makedirs(new_folder)
+
+with open(filter_file, 'r') as file:
+    files_to_move = json.load(file)
+
+for file_name in files_to_move:
+    org_path = os.path.join(org_folder, file_name)
+    new_path = os.path.join(new_folder, file_name)
+    
+    # Sprawdzenie, czy plik istnieje w folderze źródłowym
+    if os.path.exists(org_path):
+        # Przenoszenie pliku
+        shutil.move(org_path, new_path)
+        print(f"moved: {file_name}")
+    else:
+        print(f"file doesn't exist: {file_name}")
